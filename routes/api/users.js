aa965c51d2dda2033ba199c509ddffaa36aa88bf
Ken Guie <nyhunter77@yahoo.com>
2018-05-12 14:37:11 -0400

    use Gravatar for avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+
+// Load the User model
+const User = require('../../models/User');
 
 // using below actually routes to /api/users/test - reminder
 
@@ -7,5 +11,30 @@ const router = express.Router();
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works'}));  // return JSON data
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: 'Email already exists'});
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // size
+          r: 'pg', // rating
+          d: 'mm' // default image 
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+      }
+    })
+});
 
 module.exports = router;
