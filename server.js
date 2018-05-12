f28fee9c81c1839dd6debd75a17e6de9d716191b
Ken Guie <nyhunter77@yahoo.com>
2018-05-12 13:29:17 -0400

    connected up Mongo DB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB 
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo DB via mongoose and promise
+mongoose
+  .connect(db)
+  .then(() => console.log('Mongo DB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hi!'));
 
